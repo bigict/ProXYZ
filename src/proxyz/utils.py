0e70bb8e643bf33a6ef6204d0bb538aaafea5d06
chungongyu <chungongyu@gmail.com>
2026-07-15 18:31:45 +0800

    feat: train FIM as SFT (DeepSeek-Coder style)

--- src/proxyz/utils.py
+++ src/proxyz/utils.py
@@ -1,3 +1,10 @@
+import functools
+
+
 class dict2object(object):
     def __init__(self, **args):
         self.__dict__.update(args)
+
+
+def compose(*funcs):
+  return functools.reduce(lambda g, f: lambda x: f(g(x)), funcs)

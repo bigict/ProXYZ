fa00e324c1ba12ac401c0b7454572834797f3cce
chungongyu <chungongyu@gmail.com>
2026-09-20 17:05:55 +0800

    fix: install_env.sh transformers==5.12.0

--- install_env.sh
+++ install_env.sh
@@ -35,7 +35,7 @@ conda run -n ${name} \
 conda run -n ${name} \
     pip install torch_geometric==2.8.0.post1 -f https://data.pyg.org/whl/torch-2.11.0+cu128.html
 conda run -n ${name} \
-    pip install accelerate==1.14.0 datasets==5.0.0 tokenizers==0.23.2 transformers==5.17.0
+    pip install accelerate==1.14.0 datasets==5.0.0 tokenizers==0.22.2 transformers==5.12.0
 conda run -n ${name} \
     pip install graphein==1.7.8 biotite==1.6.0 foldcomp==1.0.0 lightning==2.6.5
 conda run -n ${name} \
